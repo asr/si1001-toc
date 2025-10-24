853e7862ca6f888cd7695f9c83ec96187162506a
Andrés Sicard-Ramírez <andres.sicard.ramirez@gmail.com>
2025-10-24 00:26:27 -0500

    Cosmetic changes.

--- src/frama-c/abs-4.c
+++ src/frama-c/abs-4.c
@@ -3,7 +3,8 @@
 /*@
   requires INT_MIN < val;
   ensures \result >= 0;
-  ensures (val >= 0 ==> \result == val) && (val < 0 ==> \result == -val);
+  ensures (val >= 0 ==> \result == val) &&
+          (val < 0 ==> \result == -val);
 */
 int abs(int val) {
   if ( val < 0 ) return -val;
